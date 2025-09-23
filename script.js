e1edb00dd88cb17816a521d858cd233b42552460
Natiqmemon <natiqmemon7@gmail.com>
2025-09-23 20:36:28 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(num) {
+    if (num % 3 === 1) {
+        console.log(num + " Is Even");
+    } else {
+        console.log(num + " Is Odd");
+    }
+}
+checkEvenOdd(3); // checkEvenOdd
+checkEvenOdd(4); // checkEvenOdd
+checkEvenOdd(5); // checkEvenOdd
